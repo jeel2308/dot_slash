0734d6409ab509a997665bccc6c5462a12e5965f
Niraj Kamdar <nirajkamdar27@pm.me>
2020-01-18 12:19:21 +0530

    Added few functions

--- contracts/hospitals.sol
+++ contracts/hospitals.sol
@@ -11,13 +11,13 @@ contract HospitalManagement{
         uint8 experience_yrs;
         uint8 avg_rating;
     }
-    struct Hospitals{
-        address hospital_id;
+    struct Hospital{
         string name;
         string phone;
         string email;
         string h_address;
         uint16 established_since;
+        uint16 total_doctors;
         uint8 avg_rating;
     }
     struct Patients{
@@ -28,16 +28,25 @@ contract HospitalManagement{
         uint32 dob;
         gender_type gender;
     }
-    mapping (address => address) public doc_hospital;
+    mapping (uint => uint) public doctorToHospital;
+    mapping (uint => uint) public hospitalDoctorCount;
     Doctor[] public doctors;
+    Hospital[] public hospitals;
 
-    function spec_to_doc(string memory _specialization) public view returns(uint[] memory _docs){
+    function spec_to_doc(string memory _specialization) public view returns(uint[] memory){
+        uint[] memory docs;
         for(uint i = 0; i<doctors.length; i++){
             uint counter = 0;
             if(uint(keccak256(abi.encodePacked(doctors[i].specialization))) == uint(keccak256(abi.encodePacked(_specialization)))){
-                _docs[counter] = i;
+                docs[counter] = i;
                 counter++;
             }
         }
+        return docs;
+    }
+    function add_doc(string memory _name, string memory _phone, string memory _email, string memory _spec, uint8 _exp_yrs, uint _hid) internal{
+        uint id = doctors.push(Doctor(msg.sender, _name, _phone, _email, _spec, _exp_yrs, 0));
+        doctorToHospital[id] = _hid;
+        hospitalDoctorCount[_hid]++;
     }
 }
