8706446187bc76b3ea5ddb102a7c84a3280ad12b
Niraj Kamdar <nirajkamdar27@pm.me>
2020-01-19 15:06:45 +0530

    Update myapp.py and patient.ui

--- gui/myapp.py
+++ gui/myapp.py
@@ -2,9 +2,16 @@ import sys
 import MedicalReport as ehr
 from PyQt5.QtWidgets import QApplication
 from PyQt5 import uic
+import datetime
 
 form1, base1 = uic.loadUiType("front.ui")
 form2, base2 = uic.loadUiType("patient.ui")
+form3, base3 = uic.loadUiType("patient_dashboard.ui")
+form4, base4 = uic.loadUiType("doctor.ui")
+form5, base5 = uic.loadUiType("doctor_dashboard.ui")
+form6, base6 = uic.loadUiType("hospital.ui")
+
+patient_data = {}
 
 class PatientProfile(base2, form2):
     def __init__(self):
@@ -12,10 +19,27 @@ class PatientProfile(base2, form2):
         self.setupUi(self)
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
+        
     def accept(self):
-        pass
-    def reject(pass):
-        pass
+        patient_data["name"] = self.name.text()
+        patient_data["address"] = self.address.text()
+        patient_data["phone"] = self.phone.text()
+        patient_data["email"] = self.email.text()
+        patient_data["date"] = self.dateEdit.date().toPyDateTime().strftime('%s')
+        if(self.male):
+            patient_data["gender"] = "Male"
+        elif(self.female):
+            patient_data["gender"] = "Female"
+        else:
+            patient_data["gender"] = "Other"
+             
+    def reject(self):
+        pass # redirect to dashboard
+    
+class PatientDashboard(base3, form3):
+    def __init__(self):
+        super(base1, self).__init__()
+        self.setupUi(self)
 
 class FrontPage(base1, form1):
     def __init__(self):
@@ -25,13 +49,25 @@ class FrontPage(base1, form1):
         self.doctorButton.clicked.connect(self.doctor_redirect)
     
     def patient_redirect(self):
-        pass
-        # d = ehr.show_patient_profile()
-        # if(d):
-        #     self.patientTextBrowser = PatientProfile(self)
-        #     self.dialogTextBrowser.exec_()
+        d = ehr.show_patient_profile()
+        if(d):
+            self.patientpage = PatientDashboard()
+            self.patientpage.show()
+            self.hide()
+        else:
+            self.patientpage = PatientProfile()
+            self.patientpage.show()
+            self.hide()
     def doctor_redirect(self):
-        pass
+        d = ehr.show_doctor_profile()
+        if(d):
+            self.doctorpage = DoctorDashboard()
+            self.doctorpage.show()
+            self.hide()
+        else:
+            self.doctorpage = DoctorProfile()
+            self.doctorpage.show()
+            self.hide()
     
 if __name__ == '__main__':
     app =   QApplication(sys.argv)
